50e56ce9a7c1694de4c4a1b7287ba42fbc4ac17c
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-05-14 11:04:08 -0400

    Update scripts.js
    added for loop to display pokemon name and height with conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 let pokemonList=[
-    {name: 'Bulbasaur', type: 'grass, poison', height: '2.04'}
-    {name: 'Ivysaur', type: 'grass, poison', height: '3.03'}
-    {name: 'Venusaur', type: 'grass, poison', height: '6.07'}
+    {
+        name: 'Bulbasaur ',
+        type: 'grass, poison',
+        height: '2.04'
+    },
+    {
+        name: 'Ivysaur ',
+        type: 'grass, poison',
+        height: '3.03'
+    },
+    {
+        name: 'Venusaur ',
+        type: 'grass, poison',
+        height: '6.07'
+    }
 ]
+
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 3 && pokemonList[i].height < 6){
+        console.log(pokemonList[i].name + pokemonList[i].height);
+    }else if (pokemonList[i].height >= 6){
+        console.log(pokemonList[i].name + pokemonList[i].height + " That's huge!");
+    }else if (pokemonList[i].height <= 2){
+        console.log(pokemonList[i].name + pokemonList[i].height + " That's tiny!")
+    }
+}
