d2af494df8c9abddf9aea195d84ea255bdb3e583
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-06-10 08:41:57 -0400

    wrapped pokemonList with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
-let pokemonList=[
-    {
-        name: 'Bulbasaur ',
-        type: 'grass, poison',
-        height: '2.04'
-    },
-    {
-        name: 'Ivysaur ',
-        type: 'grass, poison',
-        height: '3.03'
-    },
-    {
-        name: 'Venusaur ',
-        type: 'grass, poison',
-        height: '6.07'
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList=[
+        {
+            name: 'Bulbasaur ',
+            type: 'grass, poison',
+            height: '2.04'
+        },
+        {
+            name: 'Ivysaur ',
+            type: 'grass, poison',
+            height: '3.03'
+        },
+        {
+            name: 'Venusaur ',
+            type: 'grass, poison',
+            height: '6.07'
+        }
+    ];
 
-pokemonList.forEach(pkmn => console.log(pkmn));
+    function getAll () {
+        return pokemonList;
+    }
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})()
+
+console.log(pokemonRepository.getAll())
