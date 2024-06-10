1e6434d9358340957afb974ad30b56fb4e408f3d
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-06-10 08:27:54 -0400

    switched method to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,6 @@ let pokemonList=[
         type: 'grass, poison',
         height: '6.07'
     }
-]
+];
 
-for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height > 3 && pokemonList[i].height < 6){
-        console.log(pokemonList[i].name + pokemonList[i].height);
-    }else if (pokemonList[i].height >= 6){
-        console.log(pokemonList[i].name + pokemonList[i].height + " That's huge!");
-    }else if (pokemonList[i].height <= 2){
-        console.log(pokemonList[i].name + pokemonList[i].height + " That's tiny!")
-    }
-}
+pokemonList.forEach(pkmn => console.log(pkmn));
