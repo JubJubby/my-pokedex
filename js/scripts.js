7a5cceba1bf34e358e989a81cf1d9d70b1d35804
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-06-12 12:12:11 -0400

    create buttons for each object in repository that show details when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -23,12 +23,36 @@ let pokemonRepository = (function () {
     function add (pokemon) {
         pokemonList.push(pokemon);
     }
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.addEventListener('click', function(){
+            showDetails(pokemon);
+        });
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
+    }
 
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     }
 
 })()
 
-console.log(pokemonRepository.getAll())
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: "electric"});
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    
+    pokemonRepository.addListItem(pokemon);
+});
