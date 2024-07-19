20ec51d213e9832b0a73c6a8cda0b58cb41c6c02
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-07-19 04:27:49 -0400

    remove unnecessary code and remove second "height" display in modal

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ let pokemonRepository = (function () {
 
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1302';
-    let modalContainer = document.querySelector("#modal-container");
 
     function getAll () {
         return pokemonList;
@@ -17,11 +16,11 @@ let pokemonRepository = (function () {
         let button = document.createElement("button");
 
         listPokemon.classList.add("list-group-item");
+        
         button.classList.add("btn-dark");
         button.setAttribute("data-toggle", "modal");
         button.setAttribute("data-target", "#pokemonModal");
         button.innerText = pokemon.name;
-        // button.classList.add("button-class");
         button.addEventListener('click', function() {
             showDetails(pokemon);
         });
@@ -38,12 +37,18 @@ let pokemonRepository = (function () {
     function showModal(pokemon) {
         let modalTitle = document.querySelector(".modal-title");
         let modalBody = document.querySelector(".modal-body");
-        // let modalHeader = document.querySelector(".modal-header");
+        
+        let titleElement = document.createElement("h5");
+        titleElement.innerText = "Pokemon: " + pokemon.name;
 
+        let heightElement = document.createElement("div");
+        heightElement.innerText = "Height: " + pokemon.height;
 
-        // modalTitle.empty();
-        // modalBody.empty();
-        // modalHeader.empty();
+        let imageElement = document.createElement("img");
+        imageElement.src = pokemon.imageUrl;
+
+        let typesElement = document.createElement("div");
+        typesElement.innerText = "Type(s): " + pokemon.types;
 
         modalTitle.innerText = "Pokemon: " + pokemon.name;
 
@@ -52,57 +57,10 @@ let pokemonRepository = (function () {
             pokemon.imageUrl;
             "Type(s): " + pokemon.types;
 
-        // let modal = document.createElement("div");
-        // modal.classList.add("modal");
-
-        // let closeButton = document.querySelector("button-close");
-        // closeButton.classList.add("modal-close");
-        // closeButton.innerText = "Close";
-        // closeButton.addEventListener("click", closeModal);
-        // window.addEventListener("keydown", (e) => {
-        //     let modalContainer = document.querySelector("#modal-container");
-        //     if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
-        //         closeModal();
-        //     }
-        // });
-        // modalContainer.addEventListener("click", (e) => {
-        //     let target = e.target;
-        //     if (target === modalContainer) {
-        //         closeModal();
-        //     }
-        // });
-
-        let titleElement = document.createElement("h5");
-        // titleElement.classList.add("h1");
-        titleElement.innerText = "Pokemon: " + pokemon.name;
-
-        let heightElement = document.createElement("div");
-        // heightElement.classList.add("modal-body");
-        heightElement.innerText = "Height: " + pokemon.height;
-
-        let imageElement = document.createElement("img");
-        // imageElement.classList.add("modal-body")
-        imageElement.src = pokemon.imageUrl;
-
-        let typesElement = document.createElement("div");
-        // typesElement.classList.add("modal-body")
-        typesElement.innerText = "Type(s): " + pokemon.types;
         
-        // modalBody.appendChild(closeButton);
-        // modalBody.appendChild(titleElement);
         modalBody.appendChild(typesElement);
         modalBody.appendChild(imageElement);
-        modalBody.appendChild(heightElement);
-
-        // modalContainer.innerText = "";
-        // modalContainer.appendChild(modal);
-
-        // modalContainer.classList.add("is-visible");
     }
-
-    // function closeModal() {
-    //     modalContainer.classList.remove("is-visible");
-    // }
 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
